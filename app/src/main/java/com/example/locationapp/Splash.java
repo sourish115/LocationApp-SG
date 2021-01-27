e5809274697c7616ca3ebdc09e6b246637735418
sourish ghosh <sourish115@live.com>
2021-01-27 21:48:16 +0530

    splash screen added, UI changes

---
+++ app/src/main/java/com/example/locationapp/Splash.java
@@ -0,0 +1,26 @@
+package com.example.locationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler(Looper.myLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
